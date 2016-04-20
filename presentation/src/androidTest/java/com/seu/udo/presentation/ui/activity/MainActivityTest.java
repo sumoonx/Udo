1decc2a36e7513fd80a0b57805768136745873f5
Jeremy Xu <694726071@qq.com>
2016-04-20 22:20:36 +0800

    fix model package

--- presentation/src/androidTest/java/com/seu/udo/presentation/ui/activity/MainActivityTest.java
+++ presentation/src/androidTest/java/com/seu/udo/presentation/ui/activity/MainActivityTest.java
@@ -2,6 +2,7 @@ package com.seu.udo.presentation.ui.activity;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
 
@@ -23,11 +24,25 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  * Author: Jeremy Xu on 2016/4/20 20:29
  * E-mail: jeremy_xm@163.com
  */
-@RunWith(AndroidJUnit4.class)
-@LargeTest
-public class MainActivityTest {
-    @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule(MainActivity.class);
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mainActivity;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        setActivityIntent(MainActivity.getCallingIntent(getInstrumentation().getTargetContext()));
+        mainActivity = getActivity();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
     @Test
     public void testLogin() {
